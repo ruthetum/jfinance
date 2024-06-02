c30a5e19585ade57ea45119ac33f035486eb6bb4
Heedong <59307414+ruthetum@users.noreply.github.com>
2024-06-02 14:59:37 +0900

    chore: starter module setup

---
+++ stock/starter/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    api(project(":util:model"))
+    implementation(project(":util:calendar"))
+    implementation(project(":stock:dart"))
+    implementation(project(":stock:quant"))
+    implementation(project(":stock:yahoo"))
+}
